1775a9b5dbb559de1af2f4b012d5c0446512e763
lalagodara <69674552+lalagodara@users.noreply.github.com>
2020-08-14 15:48:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCLB0m2xnLti6RO_2WPoBocyIF_xMpDqEc",
+  authDomain: "sanju-9af28.firebaseapp.com",
+  databaseURL: "https://sanju-9af28.firebaseio.com",
+  projectId: "sanju-9af28",
+  storageBucket: "sanju-9af28.appspot.com",
+  messagingSenderId: "163198968653",
+  appId: "1:163198968653:web:fa4f512779603ad615cd04",
+  measurementId: "G-WWVGCH201C"
 };
 
      
